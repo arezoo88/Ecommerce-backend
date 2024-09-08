6a811c177c5be312d7c9de4042bf2705a036feef
Arezoo <arezoo.darvishi69@yahoo.com>
2024-09-08 12:43:17 +0330

    create custom user model with email field

---
+++ source/apps/account/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+from apps.account.managers import CustomUserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        _("email address"),
+        unique=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
