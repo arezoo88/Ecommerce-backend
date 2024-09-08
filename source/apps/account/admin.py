298159f0c829720908ebef1378c5f4e5b8e121f4
Arezoo <arezoo.darvishi69@yahoo.com>
2024-09-08 15:13:00 +0330

    customize django admin

--- source/apps/account/admin.py
+++ source/apps/account/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+from apps.account.models import Profile
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=('full_name','gender','user_email')
+    def user_email(self,obj):
+        return obj.user.email
+
+class UserAdmin(admin.ModelAdmin):
+        list_display=('email',)
+
+    
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
+
+    
