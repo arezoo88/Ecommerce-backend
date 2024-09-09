deae427837e719f845aaa45663f6a8b0e1f41100
Arezoo <arezoo.darvishi69@yahoo.com>
2024-09-09 12:08:32 +0330

    include account url in base url

--- source/base/urls.py
+++ source/base/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('apps.account.api.v1.urls', namespace='account.v1')),
 ]
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
